5a303204fe0b27ff143be274645eef08f7be9392
fatihcetin28 <fatihcetin.hd@gmail.com>
2023-08-20 02:26:33 +0300

    ButtonImages object designed looks good

---
+++ ButtonImages.py
@@ -0,0 +1,38 @@
+import DetectImageLocation as dti
+import pyautogui
+import JAWSFSAPI as JF
+from imageToText import toText
+
+class ButtonImages:
+    def __init__(self, file_path, description):
+        self.file_path = file_path
+        self.description = description
+
+    def locate_center(self):
+       return dti.image_location_center(self.file_path)
+        # locate the center of image
+
+    def locate_topleft(self):
+        return dti.image_location_position(self.file_path)
+        #  locate the position of image
+
+    def click(self):
+        if self.is_image_exist_on_window:
+            pyautogui.click(self.file_path)
+            JF.Speak(f"{self.description} düğmesi tıklandı.")
+        else:
+            print("Button not found on the window.")
+            JF.Speak(f"{self.description} düğmesi ekranda bulunamadı. Çıkış yapılıyor. Lütfen Tekrar deneyiniz.")
+    
+    def to_text(self):
+        return toText(self.file_path)
+
+    def text_to_speech(self):
+        JF.Speak(self.to_text())
+        pass
+
+    def is_image_exist_on_window(self):
+        if self.locate_center is not None:
+            return True
+        else:
+            return False
---
+++ speechs.py
@@ -0,0 +1,31 @@
+
+# handle_odunc_uyari modülü
+uyari = "Program uyarı verdi."
+
+uyari_islemden_cikis = "Uyarı verildi ödünç verme işleminden çıkılıyor."
+
+tc_demirbas_gecerli = 'Uyari Yok TC Numarası ve demirbaş numaralar geçerli görünüyor.'
+
+# İade Al Modülü
+
+iade_demirbasNoGiriniz = "Lütfen Demirbaş Seri No Girdikten sonra iade alma işlemi için entera, çıkış için ESC'ye basınız."
+
+iade_success = "İade alma işlemi başarılı."
+
+# Odunc Ver Modul
+
+odunc_tc_no_giriniz = "Lütfen TC Kimlik No Girdikten sonra devam etmek için entera, çıkış için ESC'ye basınız."
+
+odunc_tc_no_giriniz2 = "Lütfen TC Kimlik No Giriniz."
+
+odunc_demirbasNoGiriniz = "Lütfen Demirbaş Seri No Girdikten sonra ödünç işlemi için entera, çıkış için ESC'ye basınız."
+
+odunc_success= "Ödünç verme işlemi başarılı"
+
+# Open Yordam BT
+
+yordam_bt_acilis = "Yordam BT Açılıyor"
+
+yordam_bt_acildi = "Yordam BT Açıldı."
+
+
